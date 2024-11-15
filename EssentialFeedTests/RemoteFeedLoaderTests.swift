77c63810edc177b0d497671d2245828fe9ed32fa
Muhammad Nobel Shidqi <mnobelsh@gmail.com>
2024-11-16 00:41:15 +0700

    Requests data from URL on load using singleton.

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -7,29 +7,38 @@
 
 import XCTest
 
+class RemoteFeedLoader {
+    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "any-url")
+    }
+    
+}
+
+class HTTPClient {
+    static let shared: HTTPClient = HTTPClient()
+    
+    private init() {}
+    
+    var requestedURL: URL?
+}
+
 final class RemoteFeedLoaderTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_init_doesNotRequestsDataFromURL() {
+        let client = HTTPClient.shared
+        _ = RemoteFeedLoader()
+
+        XCTAssertNil(client.requestedURL)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    func test_load_requestsDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }

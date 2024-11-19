430d7fbda891a61440126596ac7fd5fb1fda04e8
Muhammad Nobel Shidqi <mnobelsh@gmail.com>
2024-11-20 00:18:11 +0700

    `LocalFeedLoader` save method performs cache deletion.

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,9 +6,14 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 final class LocalFeedLoader {
@@ -17,15 +22,42 @@ final class LocalFeedLoader {
     init(store: FeedStore) {
         self.store = store
     }
+    
+    func save(_ feed: [FeedItem]) {
+        store.deleteCachedFeed()
+    }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
 
     func test_init_doesNotDeletesCacheUponCreation() {
         let store = FeedStore()
-        let sut = LocalFeedLoader(store: store)
+        _ = LocalFeedLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
 
+}
+
+// MARK: - Helpers
+private extension CacheFeedUseCaseTests {
+    
+    func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "description", location: "location", imageURL: anyURL())
+    }
+    
+    func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
+    }
+    
 }

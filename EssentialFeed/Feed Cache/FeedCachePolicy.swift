06212bf87ba3ee9e0426e4cc9faba8410e9f31e5
Muhammad Nobel Shidqi <mnobelsh@gmail.com>
2024-11-23 19:08:39 +0700

    Make `FeedCachePolicy` internal.

--- EssentialFeed/Feed Cache/FeedCachePolicy.swift
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public final class FeedCachePolicy {
+final class FeedCachePolicy {
     private init() {}
     
     private static let calendar = Calendar(identifier: .gregorian)

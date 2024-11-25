4abd270c323aeaec98558cc3d07712690c877c43
Muhammad Nobel Shidqi <mnobelsh@gmail.com>
2024-11-26 01:30:12 +0700

    Move `ManagedFeedCache` request and mapping to shared helper methods.

--- EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -13,3 +13,16 @@ class ManagedCache: NSManagedObject {
     @NSManaged var timestamp: Date
     @NSManaged var feed: NSOrderedSet
 }
+
+extension ManagedCache {
+    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+        guard let entityName = entity().name else { return nil }
+        let request = NSFetchRequest<ManagedCache>(entityName: entityName)
+        request.returnsObjectsAsFaults = false
+        return try context.fetch(request).first
+    }
+    
+    var localFeed: [LocalFeedImage] {
+        feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+    }
+}

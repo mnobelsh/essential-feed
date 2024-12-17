e174def2543a98a9f77263a173295251a8b38d8e
Muhammad Nobel Shidqi <mnobelsh@gmail.com>
2024-12-18 00:49:25 +0700

    [MVP] Move from property injection to constructor injection in the `FeedPresenter` to guarantee presenter instance has access to it's dependencies.

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -13,12 +13,14 @@ public final class FeedUIComposer {
     private init() {}
     
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let presenter = FeedPresenter()
-        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader, presenter: presenter)
+        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
         let refreshController = FeedRefreshViewController(delegate: presentationAdapter)
         let feedViewController = FeedViewController(refreshController: refreshController)
-        presenter.feedView = FeedViewAdapter(controller: feedViewController, imageLoader: imageLoader)
-        presenter.loadingView = WeakRefVirtualProxy(refreshController)
+        
+        presentationAdapter.presenter = FeedPresenter(
+            feedView: FeedViewAdapter(controller: feedViewController, imageLoader: imageLoader),
+            loadingView: WeakRefVirtualProxy(refreshController)
+        )
         
         return feedViewController
     }
@@ -58,22 +60,21 @@ private final class FeedViewAdapter: FeedView {
 
 private final class FeedLoaderPresentationAdapter: FeedRefreshViewControllerDelegate {
     private let feedLoader: FeedLoader
-    private let presenter: FeedPresenter
+    var presenter: FeedPresenter?
     
-    init(feedLoader: FeedLoader, presenter: FeedPresenter) {
+    init(feedLoader: FeedLoader) {
         self.feedLoader = feedLoader
-        self.presenter = presenter
     }
     
     func didRequestFeedRefresh() {
-        presenter.didStartLoadingFeed()
+        presenter?.didStartLoadingFeed()
         
         feedLoader.load { [weak self] result in
             switch result {
             case let .success(feed):
-                self?.presenter.didFinishLoadingFeed(with: feed)
+                self?.presenter?.didFinishLoadingFeed(with: feed)
             case let .failure(error):
-                self?.presenter.didFinishLoadingFeed(with: error)
+                self?.presenter?.didFinishLoadingFeed(with: error)
             }
         }
     }
